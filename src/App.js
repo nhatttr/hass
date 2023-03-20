bb5ee4a1984a98abdf9fb6954a9bcda086470d17
nhatttr <105622439+nhatttr@users.noreply.github.com>
2023-03-21 00:09:45 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Họp <xong>src/App.js</code> rồi thì đi nhậu thôi AE.
         </p>
         <a
           className="App-link"
